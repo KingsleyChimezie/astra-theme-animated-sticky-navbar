47c460307424153596d5f4dc7e0f4af4aebf79ba
Kingsley Chimezie <kingsleychimezie92@gmail.com>
2020-11-03 20:13:14 +0000

    Script code completed - final draft. Preparing for intial release.

--- README.md
+++ README.md
@@ -1,55 +1,78 @@
-# KMP README TEMPLATE
-Description - enter a short description
+# Astra Theme Animated Sticky Nav Bar / Header
 
-## Prerequisites
-* prereq 1
-* prereq 2
+## By Kingsley Chimezie ##
+
+### Intro 🎤 ###
+Astra offers a wide range of WordPress themes.  
+All the free Astra themes do not currently give you the option to have a sticky navigation bar.
+
+Initially, I wrote the code for this project to help me tackle this problem on the WordPress websites I build and/or built using an Astra theme.  
+After doing some quick Googling, I noticed that there are not many solutions like mine. That's why I decided to release my code as a project.  
+For now, I think it's the best solution out there 😁.
+
+### The Project 📦 ###
+- My code will allow you to easily integrate a clean and animated 'sticky-icky' feature with your existing Astra nav bar 👌🏿
+- All you have to do is set your variables within my code and everything will be automatically integrated
+- A clear instructional video will be available on my YouTube channel [Software Basics](https://youtube.com/SoftwareBasics)
+
+### Prerequisites ✅ ###
+* [Simple Custom CSS and JS](https://wordpress.org/plugins/custom-css-js/) - Install this plugin, it will allow you to embed my code on your WordPress site.
 
 
-## Instructions
-
-### Video Guide
+### Video Guide 🎥 ###
 [![<Enter Alt Text - KMP>](http://img.youtube.com/vi/xgejrQbZO7o/0.jpg)](https://youtu.be/xgejrQbZO7o "KMP Website initialiser setup guide")
 
-### Written Guide
-1. Step 1   
-***"instructions"***  
-```Multi link code```
+### Written Guide 📃 ###
+1. Complete the Prerequisites above - install and activate the 'Simple Custom CSS and JS' plugin
+2. In your WordPress admin dashboard side panel, click 'Custom CSS & JS'
+3. Click 'Add JS Code' at the top
+4. Give your title the following name: astra-theme-animated-sticky-navbar.js
+5. Edit your permalink and also give it the following name: astra-theme-animated-sticky-navbar.js
+6. Clear all the boilerplate code in the script input field just below the permalink
+7. On the right hand side select the following options:
+   - Linking type = Internal
+   - Where on page = Footer
+   - Where in site = In Frontend
+8. Go into the 'src' folder in this repository
+9. Copy all the code in 'astra-theme-animated-sticky-navbar.js'
+10. Go back to WordPress, paste the code in the script input field and click publish
 
+### License 📜 ###
+- My code is free to use for your personal / commercial projects
+- Selling my code or claiming it as your own is strictly prohibited ⚠
+- Do not remove my credits and give credit where credit is due  
 
-2. Step 2  
+See LICENSE.md
 
-- 2.2
+### Premium Support 💻 ###
+If you need help with any integration or issues, I can give you dedicated remote support for a small fee.  
+First contact me via email only --> [📧](mailto:info@kingsley.tech?subject=Support%20For%20Astra%20Theme%20Animated%20Sticky%20Nav%20Bar)
 
-- 2.3  
+### Donations 💶 ###
+Astra might make some future changes that could cause my code to not work well (or at all) with your Astra theme.  
+Donations will allow me to keep my code updated and functional for everyone.
 
+### Future Plans 📓 ###
+I'm hoping to build this out into a WordPress plugin.  
+I wanted to start out this way but I'm just too busy for what it's currently worth.  
+Make it worth while by donating 😉.  
 
----
-## License
-* MIT - Copyright (c) 2019 Kingsley Chimezie - Kingsley Media Productions
+### Project's Blog ✍🏿 ###
+[Click here to visit the discussion blog for this project.](https://kingsleychimezie.weebly.com/blog/astra-theme-animated-sticky-nav-bar)
 
-## Project People
+### Acknowledgments 👏 ###
+* SilkyPress.com - Simple Custom CSS and JS plugin
+
+### Versions 🔢 ###
+- [Click here](https://bitbucket.org/KingsleyChimezie/astra-theme-animated-sticky-navbar/downloads/?tab=tags)
+
+### Project People 👥 ###
 | Name                	|  Title              	|  Authority Level      | Website                  	|
 |-------------------	| -------------------	| -------------------	|--------------------------	|
 | Kingsley Chimezie 	|  Author +        	    |  Admin +        	    | http://www.kingsley.tech 	|
 
 [Click here for **title** and **authority level** definitions.](https://bitbucket.org/kingsleymedia-team/kmp-repo-template/src/master/copy-this-and-use-as-repo/assets/title.md)
 
-## Acknowledgments
-*
-## Versions
-- [Click here](https://url-where-versions-are-kept)
-
-
-
-## References
-* Title  
-http://enter-link.com  
-[ACCESSED: dd.mm.yyyy]
-
-* Title  
-http://enter-link.com  
-[ACCESSED: dd.mm.yyyy]
-* Title  
-http://enter-link.com  
-[ACCESSED: dd.mm.yyyy]
+[![Donate](https://resources.kingsley.tech/images/donate_paypal_kingsley_chimezie_markdown.png)](https://www.paypal.com/paypalme/kingsleychimezie/10eur)
+---
+© 2020 Kingsley Chimezie | [Kingsley Chimezie Creations](https://kingley.tech) | [Kingsley Media Productions](https://kingsleymedia.ie)
--- src/astra-theme-animated-sticky-navbar.js
+++ src/astra-theme-animated-sticky-navbar.js
@@ -1,45 +1,52 @@
 /*
-** DATE:   
-            Created 2020.10.27 - Republic of Ireland
-** AUTHOR:  
-            Kingsley Chimezie
-            Software Engineer | Web Developer | Content Creator
-** URLs:    
-            https://kingsley.tech           (Social)
-            https://kingsleymedia.ie        (Business)
+DATE:   
+          Created 2020.10.27 - Republic of Ireland
+AUTHOR:  
+          Kingsley Chimezie
+          Software Engineer | Web Developer | Content Creator
+URLs:    
+          https://kingsley.tech           (Social)
+          https://kingsleymedia.ie        (Business)
+
+- My code is free to use for your personal / commercial projects
+- Selling my code or claiming it as your own is strictly prohibited ⚠
+- Do not remove my credits and give credit where credit is due
+
+Copyright © 2020 Kingsley Chimezie | Kingsley Chimezie Creations | Kingsley Media Productions
 */ 
 
 
-// Custom variables
-var navbarPaddingDefault = "16px 0px";            // Enter your navbar padding | 16px = top & bottom, 0px = left & right
-var navbarPaddingShrink = "0px";                  // Enter the padding size you want your navbar to shrink to
-var navbarColorOnScroll = "";	                    // Set the colour you want your navbar to be when you scroll, leave blank for default '(accepted colour format: hex/rgb/rgba)
-var animationSpeed = "0.2s";                      // How fast or slow you want the shrink animation to be
-var scrollDistance = 80;                          // How far do you want to scroll before the activating sticky nav bar. DO NOT enter 'px', just number.
-var siteContentTopPaddingDesktop = "0px";         // I recommend you set this to the height of your navbar (in px)
-var siteContentTopPaddingTablet = "0px";          // I recommend you set this to the height of your navbar (in px)
-var siteContentTopPaddingMobile = "0px";          // I recommend you set this to the height of your navbar (in px)
+// CUSTOM VARIABLES
+var navbarPaddingOnScroll = "0px";                // Enter the padding size you want your navbar to be on scroll
+var navbarColorOnScroll = "";                     // Set the colour you want your navbar to be on scroll (accepted colour format: hex/rgb/rgba)
+var animationSpeed = "0.3s";                      // How fast or slow do you want the navbar animation to be?
+var scrollDistance = 10;                          // How far do you want to scroll before activating sticky navbar? DO NOT enter 'px', just number
 
+// OPTIONAL CUSTOM VARIABLES
+var navbarPaddingDefault = "";                    // How much top-bottom left-right padding (in px) do you want the navbar?
+var siteContentTopPaddingDesktop = "";            // How much top padding (in px) do you want the site content area to have for Desktop? (> 921px width)
+var siteContentTopPaddingTablet = "";             // How much top padding (in px) do you want the site content area to have for Tablet? (<= 921px width) 
+var siteContentTopPaddingMobile = "";             // How much top padding (in px) do you want the site content area to have for Mobile? (<= 768px width)
 
-// Default/Custom variables (edit only when necessary)
+// DEFAULT VARIABLES (EDIT ONLY WHEN NECESSARY)
 var boxShadow = "0 4px 8px 0 rgba(0,0,0,0.3)";    // This adds a box shadow to the navbar, comment out (//) to disable
-var siteContentId = "main";                       // 
-var navbarClass = "main-header-bar";              // This is usually the default class in for Astra navbar
-astraMinScreenWidthTablet = 921;                  // Screen Width
-astraMinScreenWidthMobile = 768;
+var astraMinScreenWidthTablet = 921;              // Minimum screen width for tablet
+var astraMinScreenWidthMobile = 768;              // Minimum screen width for mobile
+var siteContentId = "content";                    // The ID of site content div (do not enter the class name)
+var navbarClass = "main-header-bar";              // This is usually the default class for Astra navbar
 
 
-
-/* ONLY EDIT BEYOND THIS POINT IF YOU KNOW JS OR WHAT YOU'RE DOING :)
-************************************************************************/
+/* ⚠ ONLY EDIT BEYOND THIS POINT IF YOU KNOW JAVASCRIPT :) ⚠
+******************************************************************************************************************/
 var i;
 var siteContentDiv = document.getElementById(siteContentId);
 var navBarDiv = document.getElementsByClassName(navbarClass);
 
-// On scroll call scroll function
-window.onscroll = function() {scrollFunction();};
-
-// Setting main content padding for the site's Astra theme according to screen size
+// Setting any pre-scroll defaults to navbar
+for (i = 0; i < navBarDiv.length; i++) {
+  navBarDiv[i].style.padding = navbarPaddingDefault;
+}
+// Setting site content padding according to screen size
 siteContentDiv.style.paddingTop = siteContentTopPaddingDesktop;
 if (screen.width <= astraMinScreenWidthTablet) {
   siteContentDiv.style.paddingTop = siteContentTopPaddingTablet;
@@ -48,31 +55,44 @@ if (screen.width <= astraMinScreenWidthMobile) {
   siteContentDiv.style.paddingTop = siteContentTopPaddingMobile;
 }
 
-// Setting sticky icky navbar
-for (i = 0; i < navBarDiv.length; i++) {
-  navBarDiv[i].style.padding = navbarPaddingDefault;
-  navBarDiv[i].style.position = "fixed";
-  navBarDiv[i].style.width = "100%";
-  navBarDiv[i].style.transition = animationSpeed;
-  navBarDiv[i].style.transitionTimingFunction = "ease";
-}
+// On scroll, call scroll function
+window.onscroll = function() {scrollFunction();};
 
 function scrollFunction() {
   
-  // if scroll is greater than defined scroll distance, set sticky icky navbar 👌🏿
-  if (document.body.scrollTop > scrollDistance || document.documentElement.scrollTop > scrollDistance) {    
-    // Setting sticky icky navbar when  
+  /*  if scroll is greater than your defined scroll distance, set sticky-icky navbar 👌🏿. 
+      We should only do this on scroll, so that the default navbar padding is not affected untill we scroll.
+  */
+  if (document.body.scrollTop > scrollDistance || document.documentElement.scrollTop > scrollDistance) {
+
+    // Setting sticky-icky navbar  
     for (i = 0; i < navBarDiv.length; i++) {
-      navBarDiv[i].style.padding = navbarPaddingShrink;
+      navBarDiv[i].style.position = "fixed";
+      navBarDiv[i].style.width = "100%";
+      navBarDiv[i].style.transition = animationSpeed;
+      navBarDiv[i].style.transitionTimingFunction = "ease";
+      
+      navBarDiv[i].style.padding = navbarPaddingOnScroll;
       navBarDiv[i].style.boxShadow = boxShadow;
       navBarDiv[i].style.backgroundColor = navbarColorOnScroll;
     }
-  } else {
-    // reset when back to top
+
+  } 
+  
+  //  Else when scroll is less than your defined scroll distance, reset sticky-icky navbar. 
+  else {
+  
+    // Resetting sticky-icky navbar when back to top
     for (i = 0; i < navBarDiv.length; i++) {
+      navBarDiv[i].style.position = "";
+      navBarDiv[i].style.width = "";
+      navBarDiv[i].style.transition = animationSpeed;
+      navBarDiv[i].style.transitionTimingFunction = "ease";
+      
       navBarDiv[i].style.padding = navbarPaddingDefault;
-      navBarDiv[i].style.boxShadow = "none";
+      navBarDiv[i].style.boxShadow = "";
       navBarDiv[i].style.backgroundColor = "";
     }
+
   }
 }
